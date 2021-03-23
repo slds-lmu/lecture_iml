a4f55b2498dfc1de7f7759ad6302e09c3b8689a0
Christoph Molnar <christoph.molnar@gmail.com>
2021-03-23 16:10:50 +0100

    adds scripts and data

---
+++ slides/shapley/rsrc/ggsave-7.R
@@ -0,0 +1,24 @@
+
+library(knitr)
+library(mlr)
+library(partykit)
+library(vcd)
+library(iml)
+library(gridExtra)
+
+
+set.seed(123)
+load("bike.RData")
+task = makeRegrTask(data = bike, target = "cnt")
+mod = train("regr.randomForest", task)
+predictor = Predictor$new(mod, data = bike[-which(names(bike) == "cnt")], y = bike$cnt)
+bike.x = bike[names(bike) != 'cnt']
+
+#################################################
+
+instance.index = 200
+mean.prediction = mean(predictor$predict(bike)[[1]])
+instance.prediction = predictor$predict(bike[instance.index,])[[1]]
+shap = Shapley$new(predictor, x.interest = bike.x[instance.index,], sample.size = 300)
+strongest.feature = shap$results[shap$results$phi == max(shap$results$phi),]
+plot(shap)
