151245fd173ddb5338a02b69f06c4ba18fbefd5f
susanne-207 <dandl.susanne@gmail.com>
2021-03-30 18:07:49 +0200

    First 10 slides LIME

---
+++ slides/local-explanations/rsrc/lime.R
@@ -0,0 +1,69 @@
+
+library(mlr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(dplyr)
+library(readr)
+library(randomForest)
+library(earth)
+library(MASS)
+# library(lime)
+library(foreach)
+# library(doRNG)
+library(doParallel)
+library(grDevices)
+
+# make_split <- function(data, share, seed = 100) {
+#   set.seed(seed)
+#   split <- sample(1:nrow(data), floor(share * nrow(data)))
+#   return(list(train = data[split, ], test = data[-split, ]))
+# 
+# }
+
+
+set.seed(10)
+x <- seq(0, 10, length.out = 200)
+y <- 2 * (sin(0.04* x ^ 2) + 0.1 * x) + 2
+data <- data.frame(x = x, y = y)
+
+fig0 <- ggplot(data, aes(x = x, y = y)) + 
+  geom_smooth(se = FALSE, col = "black") + 
+  geom_ribbon(aes(ymin = 0,ymax = predict(loess(y ~ x))),
+    alpha = 0.3,fill = 'lightgray') + 
+  geom_ribbon(aes(ymin = predict(loess(y ~ x)), ymax = 8),
+    alpha = 0.3,fill = 'black') + 
+  theme_bw() + ylim(c(0, 8)) +
+  ylab("x2") + xlab("x1") 
+fig0
+ggsave("../figure/lime1.pdf", plot = fig0, width = 4, height = 3)
+
+x1train <-  runif(200, 0, 10)
+x2train <-  runif(200, 0, 8)
+data_train <- data.frame(x1 = x1train, x2 = x2train)
+
+# add point to explain
+fig0 <- fig0 + geom_point(aes(x=2.8, y=3.5), colour="yellow", cex = 3)
+fig0 
+ggsave("../figure/lime2.pdf", plot = fig0, width = 4, height = 3)
+
+# add sampled points
+fig0 <- fig0 + geom_point(aes(x = x1train, y = x2train), col = "blue")
+fig0
+ggsave("../figure/lime3.pdf", plot = fig0, width = 4, height = 3)
+
+xorig <- c(2.8, 3.5)
+euclidean <- function(x) sqrt(sum((xorig - x)^2))
+
+size <- rank(-apply(data_train, 1, euclidean))
+range01 <- function(x){0.5*(x-min(x))/(max(x)-min(x))}
+size <- range01(size)
+
+# add weights
+fig0 <- fig0 + geom_point(aes(x = x1train, y = x2train, size = size, col = size)) +
+  theme(legend.title = element_blank(), legend.position = "none") + 
+  geom_abline(intercept = 2, slope = 2, color="red", 
+    linetype="dashed", size=1.5)
+fig0 
+ggsave("../figure/lime4.pdf", plot = fig0, width = 4, height = 3)
+
