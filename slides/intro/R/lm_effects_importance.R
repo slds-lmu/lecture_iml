06d869640b0a5834ceb4672bc3d8067b50a9f300
scholbeck <christian.scholbeck@gmx.de>
2021-07-09 17:58:40 +0200

    updates intro slides

--- slides/intro/R/lm_effects_importance.R
+++ slides/intro/R/lm_effects_importance.R
@@ -1,34 +1,45 @@
 library(effects)
 library(mgcViz)
-
+library(mgcv)
 
 data_bike = read.csv("slides/intro/R/day.csv")
-summary(data_bike)
+data_bike$cnt = as.numeric(data_bike$cnt)
+str(data_bike)
+
 lm_mod = lm(cnt ~ (hum + temp)^2, data = data_bike)
+sink(file = "slides/intro/figure/lm_output.txt")
 summary(lm_mod)
-
-
+sink(file = NULL)
 png("slides/intro/figure/lm_effect_plot.png", width = 12, height = 12, units = "cm", res = 1200)
 plot(Effect(c("temp"), lm_mod), type = "response")
 dev.off()
 
 
 glm_mod = glm(cnt ~ (hum + temp)^2, data = data_bike, family = Gamma(link = "inverse"))
+sink(file = "slides/intro/figure/glm_output.txt")
 summary(glm_mod)
+sink(file = NULL)
 png("slides/intro/figure/glm_effect_plot.png", width = 12, height = 12, units = "cm", res = 1200)
 plot(Effect(c("temp"), glm_mod), type = "response")
 dev.off()
 
 
-gam_mod = gam(cnt ~ s(hum) + s(temp) + s(hum, temp), data = data_bike, family = gaussian)
+gam_mod = mgcv::gam(cnt ~ s(hum, temp), data = data_bike, family = gaussian)
+sink(file = "slides/intro/figure/gam_output.txt")
 summary(gam_mod)
+sink(file = NULL)
+
 vis = getViz(gam_mod)
-png("slides/intro/figure/gam_effect_plot_1.png", width = 12, height = 12, units = "cm", res = 1200)
+png("slides/intro/figure/gam_effect_plot.png", width = 12, height = 12, units = "cm", res = 1200)
 plot(sm(vis, 1))
 dev.off()
-png("slides/intro/figure/gam_effect_plot_2.png", width = 12, height = 12, units = "cm", res = 1200)
-plot(sm(vis, 2))
-dev.off()
-png("slides/intro/figure/gam_effect_plot_3.png", width = 12, height = 12, units = "cm", res = 1200)
-plot(sm(vis, 3))
+
+
+
+mboost_mod = mboost(cnt ~ bols(hum) + bols(temp) + bspatial(hum, temp), data = data_bike)
+sink(file = "slides/intro/figure/mboost_output.txt")
+summary(mboost_mod)
+sink(file = NULL)
+png("slides/intro/figure/mboost_effect_plot.png", width = 12, height = 12, units = "cm", res = 1200)
+plot(mboost_mod)
 dev.off()
