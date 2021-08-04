5a4005bd8215463c66e0dbd37c034bdabd672e9e
gcskoenig <gunnar.koenig@pm.me>
2021-08-04 19:29:09 +0200

    added loco

---
+++ slides/feature-importance/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# cleans all latex compilation log files
+rm *.aux
+rm *.pdf
+rm *.log
+rm *.tex.tex
+rm *.snm
+rm *.fls
+rm *.toc
+rm *.fdb_latexmk
+rm *.nav
+rm *.out
+rm *.synctex*
+rm *.ps
