3348698a8310f4adc97c3b2f922e0f24b9aebe04
henrifnk <Henri.Funk@campus.lmu.de>
2022-02-22 11:28:34 +0100

    pdp feature importance

---
+++ slides/feature-importance/rsrc/pdp_deviation.R
@@ -0,0 +1,33 @@
+# PREREQ -----------------------------------------------------------------------
+
+library(patchwork)
+library(ggplot2)
+source("slides/feature-effects/rsrc/anova_bike.R")
+
+# DATA -------------------------------------------------------------------------
+
+pdp_tmp = FeatureEffect$new(pred.bike, "temp", method = "pdp")
+pdp_tmp$results$.mean =  mean(pdp_tmp$results$.value)
+pdp_ws = FeatureEffect$new(pred.bike, "windspeed", method = "pdp")
+pdp_ws$results$.mean =  mean(pdp_ws$results$.value)
+# PLOT -------------------------------------------------------------------------
+
+ptmp = pdp_tmp$plot() +
+  scale_x_continuous('Temperature', limits = c(0, NA)) +
+  geom_hline(aes(yintercept = .mean),
+             color = "red", linetype="dashed") +
+  geom_ribbon(aes(ymin=.mean,ymax=.value), fill="blue", alpha=0.5) +
+  scale_y_continuous('Predicted number of bike rentals',
+                     limits = range(pdp_tmp$results$.value)) +
+  xlim(range(bike$temp))
+
+pws = pdp_ws$plot() +
+  scale_x_continuous('Windspeed', limits = c(0, NA)) +
+  geom_hline(aes(yintercept = .mean),
+             color = "red", linetype="dashed") +
+  geom_ribbon(aes(ymin=.mean,ymax=.value), fill="blue", alpha=0.5) +
+  scale_y_continuous('Predicted number of bike rentals',
+                     limits = range(pdp_tmp$results$.value)) +
+  xlim(range(bike$windspeed))
+
+ggsave("slides/feature-importance/figure_man/pdps_dev.pdf", height = 2, width = 8, ptmp+pws)
---
+++ slides/feature-importance/rsrc/pdps_bike.R
@@ -0,0 +1,29 @@
+# PREREQ -----------------------------------------------------------------------
+
+library(patchwork)
+library(ggplot2)
+source("slides/feature-effects/rsrc/anova_bike.R")
+
+# DATA -------------------------------------------------------------------------
+
+pdp_tmp = FeatureEffect$new(pred.bike, "temp", method = "pdp")
+pdp_ws = FeatureEffect$new(pred.bike, "windspeed", method = "pdp")
+# PLOT -------------------------------------------------------------------------
+
+ptmp = pdp_tmp$plot() +
+  scale_x_continuous('Temperature', limits = c(0, NA)) +
+  geom_hline(yintercept = mean(pdp_tmp$results$.value),
+             color = "red", linetype="dashed") +
+  scale_y_continuous('Predicted number of bike rentals',
+                     limits = range(pdp_tmp$results$.value)) +
+  xlim(range(bike$temp))
+
+pws = pdp_ws$plot() +
+  scale_x_continuous('Windspeed', limits = c(0, NA)) +
+  geom_hline(yintercept = mean(pdp_ws$results$.value),
+             color = "red", linetype="dashed") +
+  scale_y_continuous('Predicted number of bike rentals',
+                     limits = range(pdp_tmp$results$.value)) +
+  xlim(range(bike$windspeed))
+
+ggsave("slides/feature-importance/figure_man/pdps_bike.pdf", height = 2, width = 8, ptmp+pws)
