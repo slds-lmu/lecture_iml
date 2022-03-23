dadff54ed062aa72d6b0c8f141f9c4607aa4e574
susanne-207 <dandl.susanne@gmail.com>
2022-03-23 18:27:18 +0100

    update first exercise sheet

--- exercises/featureeffects/ex_rnw/catale.R
+++ exercises/featureeffects/ex_rnw/catale.R
@@ -16,6 +16,7 @@ order_levels <- function(data, feature.name) {
   dists <- lapply(others, function(k) {
     feature.k <- data[, k]
     if (inherits(feature.k, "factor") | inherits(feature.k, "character")) {
+      # NEEDS TO BE IMPLEMENTED (SEE BELOW)!
       dists <- get_diff_cat(feature.k, feature)
     } else {
       dists <- get_diff_numeric(feature.k, feature)
@@ -56,10 +57,6 @@ get_diff_cat <- function(feature.k, feature.j) {
   dists <- expand.grid(unique(feature.j), unique(feature.j))
   colnames(dists) <- c("class1", "class2")
   # get relative frequency table
-  x.count <- as.numeric(table(feature.j))
-  A <- table(feature.j, feature.k) / x.count
-  dists$dist <- rowSums(abs(A[dists[, "class1"], ] - A[dists[, "class2"], ])) / 2
-  return(dists)
 }
 
 
@@ -108,48 +105,3 @@ if (FALSE) {
   order_levels(credit, "personal_status_sex")
   get_diff_cat(feature.k = credit[,"employment_duration"], feature.j = credit[,"personal_status_sex"])
 }
-
-
-
-########### ORIGINAL 
-# order_levels <- function(dat, feature.name) {
-#   feature <- dat[, feature.name, with = FALSE][[1]]
-#   x.count <- as.numeric(table(dat[, feature.name, with = FALSE]))
-#   x.prob <- x.count / sum(x.count)
-#   K <- nlevels(dat[, feature.name, with = FALSE])
-#   
-#   dists <- lapply(setdiff(colnames(dat), feature.name), function(x) {
-#     browser()
-#     feature.x <- dat[, x, with = FALSE][[1]]
-#     dists <- expand.grid(levels(feature), levels(feature))
-#     colnames(dists) <- c("from.level", "to.level")
-#     if (inherits(feature.x, "factor")) {
-#       ## Derive! 
-#       A <- table(feature, feature.x) / x.count
-#       dists$dist <- rowSums(abs(A[dists[, "from.level"], ] - A[dists[, "to.level"], ])) / 2
-#     } else {
-#       # get decentiles
-#       quants <- quantile(feature.x, probs = seq(0, 1, length.out = 10), na.rm = TRUE, names = FALSE)
-#       # derive empirical distribution function for each category
-#       ecdfs <- data.frame(lapply(levels(feature), function(lev) {
-#         x.ecdf <- ecdf(feature.x[feature == lev])(quants)
-#         return(x.ecdf)
-#       }))
-#       colnames(ecdfs) <- levels(feature)
-#       # get pairwise absolute distances of empirical distribution function 
-#       # between the different categories
-#       ecdf.dists.all <- abs(ecdfs[, dists$from.level] - ecdfs[, dists$to.level])
-#       # get maximum distance over decentiles for each pair of categories
-#       dists$dist <- apply(ecdf.dists.all, 2, max)
-#     }
-#     dists
-#   })
-#   dists.cumulated.long <- as.data.table(Reduce(function(d1, d2) {
-#     d1$dist <- d1$dist + d2$dist
-#     d1
-#   }, dists))
-#   dists.cumulated <- data.table::dcast(dists.cumulated.long, from.level ~ to.level, value.var = "dist")[, -1]
-#   diag(dists.cumulated) <- 0
-#   scaled <- cmdscale(dists.cumulated, k = 1)
-#   order(scaled)
-# }
