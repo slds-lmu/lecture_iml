4add75f79bbac42a7e7fa4f41aeab1d3da7cfcc2
giuseppec <giuseppe.casalicchio@gmail.com>
2022-04-26 21:55:34 +0200

    update figs

--- slides/feature-effects/rsrc/fig_ale1d.R
+++ slides/feature-effects/rsrc/fig_ale1d.R
@@ -8,11 +8,21 @@ source("slides/feature-effects/rsrc/anova_bike.R")
 # DATA -------------------------------------------------------------------------
 
 pdp = FeatureEffect$new(pred.bike, "hum", method = "pdp", grid.size = 50)
+pdpt = FeatureEffect$new(pred.bike, "temp", method = "pdp", grid.size = 50)
 ale = FeatureEffect$new(pred.bike, "hum", method = "ale", grid.size = 50)
+alet = FeatureEffect$new(pred.bike, "temp", method = "ale", grid.size = 50)
 
 # PLOT -------------------------------------------------------------------------
 
-pdp_plot <- pdp$plot() + scale_y_continuous('Univariate PD on humidity')
-ale_plot <- ale$plot() + scale_y_continuous('First order ALE of humidity')
+pdp_plot <- pdp$plot() + scale_y_continuous('Univariate PD of hum') +
+  xlab("hum: Humidity")
+pdp_plot_t <- pdpt$plot() + scale_y_continuous('Univariate PD of temp') +
+  xlab("temp: Temperature")
+ale_plot <- ale$plot() + scale_y_continuous('First order ALE of hum') +
+  xlab("hum: Humidity")
+ale_plot_t <- alet$plot() + scale_y_continuous('First order ALE of temp') +
+  xlab("temp: Temperature")
 
-ggsave("slides/feature-effects/figure/ale1d.pdf", pdp_plot + ale_plot, width = 8, height = 4)
+ggsave("slides/feature-effects/figure/ale1d.pdf",
+  (pdp_plot + pdp_plot_t) / (ale_plot + ale_plot_t),
+  width = 8, height = 4)
--- slides/feature-effects/rsrc/fig_ale2d.R
+++ slides/feature-effects/rsrc/fig_ale2d.R
@@ -7,12 +7,12 @@ source("slides/feature-effects/rsrc/anova_bike.R")
 
 # DATA -------------------------------------------------------------------------
 
-pdp = FeatureEffect$new(pred.bike, feature = c("temp", "hum"), method = "pdp", grid.size = 10)
-ale = FeatureEffect$new(pred.bike, feature = c("temp", "hum"), method = "ale", grid.size = 10)
+pdp = FeatureEffect$new(pred.bike, feature = c("temp", "hum"), method = "pdp", grid.size = 20)
+ale = FeatureEffect$new(pred.bike, feature = c("temp", "hum"), method = "ale", grid.size = 20)
 
 # PLOT -------------------------------------------------------------------------
 
-pdp_plot <- pdp$plot() + theme(legend.position = "top", legend.key.width = unit(1.5,"cm")) + ggtitle("Bivariate PD")
-ale_plot <- ale$plot() + theme(legend.position = "top", legend.key.width = unit(1.5,"cm")) + ggtitle("Second order ALE")
+pdp_plot <- pdp$plot() + theme(legend.position = "top", legend.key.width = unit(1.5,"cm")) + ggtitle("Bivariate PD") + scale_fill_viridis_b(expression(hat(y)))
+ale_plot <- ale$plot() + theme(legend.position = "top", legend.key.width = unit(1.5,"cm")) + ggtitle("Second order ALE") + scale_fill_viridis_b("ALE")
 
 ggsave("slides/feature-effects/figure/ale2d.pdf", pdp_plot + ale_plot, width = 8, height = 4)
