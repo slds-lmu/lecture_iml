62433de5ba43da4c93eaebe0c43446ee1dfc03e3
giuseppec <giuseppe.casalicchio@gmail.com>
2022-05-11 18:50:13 +0200

    update shap code and fig

--- slides/shapley/rsrc/shapley-bike.R
+++ slides/shapley/rsrc/shapley-bike.R
@@ -17,7 +17,7 @@ load("data/bike.RData")
 tsk = TaskRegr$new(id = "bike", backend = bike, target = "cnt")
 mod = lrn("regr.ranger")$train(tsk)
 
-pred.bike = Predictor$new(mod, data = bike)
+pred.bike = Predictor$new(mod, data = bike[,names(bike) != 'cnt'])
 mean.pred = round(mean(mod$predict(tsk)$response), 3)
 
 #################################################
@@ -25,8 +25,8 @@ mean.pred = round(mean(mod$predict(tsk)$response), 3)
 instance.index = 200
 mean.prediction = mean(pred.bike$predict(bike)[[1]])
 instance.prediction = pred.bike$predict(bike[instance.index,])[[1]]
-shap = Shapley$new(pred.bike, x.interest = bike[instance.index,], sample.size = 300)
+shap = Shapley$new(pred.bike, x.interest = bike[instance.index, names(bike) != 'cnt'], sample.size = 300)
 strongest.feature = shap$results[shap$results$phi == max(shap$results$phi),]
 p = plot(shap)
 
-ggsave("slides/shapley/figure/shapley-bike.pdf", p, width = 8, height = 5)
+ggsave("slides/shapley/figure/shapley-bike.pdf", p, width = 9, height = 4)
