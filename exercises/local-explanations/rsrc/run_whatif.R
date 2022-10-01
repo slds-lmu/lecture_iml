0b59689ce437e5f2c7fc169811f8331a5715047e
XiaohanSun01 <sunxiaohan0401@gmail.com>
2022-10-01 21:13:47 +0200

    Made figures correctly
    Make all figures correct and change the folder of "code" under the direct chapter directory.

--- exercises/local-explanations/rsrc/run_whatif.R
+++ exercises/local-explanations/rsrc/run_whatif.R
@@ -5,9 +5,9 @@ source("exercises/local-explanations/rsrc/data_whatif.R")
 # plot -----------------------------------------------------------------------
 # Compute counterfactual for first observation
 x_interest = df[1,]
+x_interest
 print(xtable::xtable(x_interest),
       file="exercises/local-explanations/rsrc/x_interest.txt")
-
 
 cf = generate_whatif(x_interest = x_interest, model = mod, dataset = df)
 print(xtable::xtable(cf),
