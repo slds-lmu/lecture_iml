ea80fd6eaf00be74af49a4b988dddc4e858d236c
giuseppec <giuseppe.casalicchio@gmail.com>
2022-10-04 16:06:47 +0200

    makefile + cleanups

--- exercises/chapter.mk
+++ exercises/chapter.mk
@@ -1,10 +1,10 @@
-EX = $(shell find . -maxdepth 1 -type f \( -iname "ex_*.Rnw" -o -iname "sol_*.Rnw" -o -iname "collection_*.Rnw" -o -iname "ic_*.Rnw" \))
-EXS = $(EX:%.Rnw=%.pdf)
+EX = $(shell find . -maxdepth 1 -type f \( -iname "ex_*.tex" -o -iname "sol_*.tex" -o -iname "collection_*.tex" -o -iname "ic_*.tex" \))
+EXS = $(EX:%.tex=%.pdf)
 
 all: texclean $(EXS) texclean copy
 
-$(EXS): %.pdf: %.Rnw
-	Rscript -e 'setwd("$(dir $<)"); knitr::knit2pdf("$(notdir $<)")'
+$(EXS): %.pdf: %.tex
+	latexmk -pdf $<
 	
 copy: 
 	find . -maxdepth 1 -type f \( -iname "ex_*.pdf" -o -iname "sol_*.pdf" -o -iname "collection_*.pdf" -o -iname "ic_*.pdf" \) -exec cp {}  ../../exercises-pdf \;
@@ -27,4 +27,5 @@ texclean:
 	rm -rf *.vrb
 	rm -rf *.synctex.gz
 	rm -rf *-concordance.tex
-	rm -rf *.tex
+	rm -rf *.fls
+	rm -rf *.fdb_latexmk
