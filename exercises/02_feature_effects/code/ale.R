005a5a62a718c05d69d3d6e5850dc25f635b9e0e
jakobCampus <123662159+jakobCampus@users.noreply.github.com>
2023-03-28 14:20:50 +0200

    Some minor changes to be inline with the model solution.

--- exercises/02_feature_effects/code/ale.R
+++ exercises/02_feature_effects/code/ale.R
@@ -1,6 +1,6 @@
 library("docstring")
 
-get_bounds = function(X, s, n_intervals = 100) {
+get_bounds <- function(X, s, n_intervals = 100) {
   
   #' Calculates interval bounds given a X's min and max values.
   #' Only values in the s-th column are considered.
@@ -15,7 +15,7 @@ get_bounds = function(X, s, n_intervals = 100) {
   return(NULL)
 }
 
-calculate_ale = function(model, X, s, n_intervals = 100, centered = FALSE) {
+calculate_ale <- function(model, X, s, n_intervals = 100, centered = FALSE) {
   
   #' Compute the accumulated local effect of a numeric continuous feature.
   #'
@@ -32,9 +32,9 @@ calculate_ale = function(model, X, s, n_intervals = 100, centered = FALSE) {
   return(list(bounds = NULL, ale = NULL))
 }
 
-prepare_ale = function(model, X, s, n_intervals = 100, centered = TRUE) {
+prepare_ale <- function(model, X, s, n_intervals = 100, centered = TRUE) {
   #' Uses `calculate_ale` to prepare x and y data, which can be used
-  #' by matplotlib directly.
+  #' by ggplot2 directly.
   #'
   #' @param model: Classifier which has a predict method.
   #' @param X (data.frame): Input data.
@@ -51,41 +51,44 @@ prepare_ale = function(model, X, s, n_intervals = 100, centered = TRUE) {
   
 }
 
-if (FALSE) {
+### A function to create the ALE plot from the previous generated objects.
+ale_plot <- function() {
   library(randomForest)
   library(ggplot2)
   
-  # Set up your working directory using swd() and get the dataset file. 
-  df = read.csv(file = 'datasets/wheat_seeds.csv')
+  ### Set up your working directory using swd() and get the dataset file. 
+  df <- read.csv(file = 'datasets/wheat_seeds.csv')
   
-  # Split the dataset to 70% train data and 30% test data.
+  ### Split the dataset to 70% train data and 30% test data.
   set.seed(100)
-  train = sample(nrow(df), 0.7 * nrow(df), replace = FALSE)
-  trainData = df[train, ]
-  testData = df[-train, ]
+  train <- sample(nrow(df), 0.7 * nrow(df), replace = FALSE)
+  trainData <- df[train, ]
+  testData <- df[-train, ]
   
-  # Normalize the target to be between 0 and 1.
-  min_max_norm = function(x) {
+  ### Normalize the target to be between 0 and 1.
+  min_max_norm <- function(x) {
     (x - min(x)) / (max(x) - min(x))
   }
   
-  trainData$Type = min_max_norm(trainData$Type)
+  trainData$Type <- min_max_norm(trainData$Type)
   
-  # Build a Random Forest model.
-  model = randomForest(Type ~ ., data = trainData, mtry = 4, importance = TRUE)
+  ### Build a Random Forest model.
+  model <- randomForest(Type ~ ., data = trainData, mtry = 4, importance = TRUE)
   
-  # Use the first feature from the dataset and set up
-  # 4 intervals to test the get_bounds function
-  bounds = get_bounds(df, 1, 4)
+  ### Use the first feature from the dataset and set up
+  ### 4 intervals to test the get_bounds function
+  bounds <- get_bounds(df, 1, 4)
   
-  # Test the calculate_ale function, with centered = FALSE
-  uncentered_ale = calculate_ale(model, df, 1, 4, FALSE)
+  ### Test the calculate_ale function, with centered = FALSE
+  uncentered_ale <- calculate_ale(model, df, 1, 4, FALSE)
   
-  #Test the prepare_ale function, with centered = TRUE
-  prepared_ale = prepare_ale(model, df, 1, 4, TRUE)
+  ### Test the prepare_ale function, with centered = TRUE
+  prepared_ale <- prepare_ale(model, df, 1, 4, TRUE)
   
   ggplot(data = prepared_ale, mapping = aes(x = x, y = y)) + 
     geom_line() + 
     geom_point()
 }
 
+### The warning message can be ignored here.
+ale_plot()
