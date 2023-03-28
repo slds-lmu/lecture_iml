8b78421bfd6929a2f668da5903dca7c96d1511ba
jakobCampus <123662159+jakobCampus@users.noreply.github.com>
2023-03-28 14:19:36 +0200

    Added comments and simplified the centering.

--- exercises/02_feature_effects/code/sol_ale.R
+++ exercises/02_feature_effects/code/sol_ale.R
@@ -1,6 +1,6 @@
 library("docstring")
 
-get_bounds = function(X, s, n_intervals = 100) {
+get_bounds <- function(X, s, n_intervals = 100) {
   
   #' Calculates interval bounds given a X's min and max values.
   #' Only values in the s-th column are considered.
@@ -10,13 +10,11 @@ get_bounds = function(X, s, n_intervals = 100) {
   #' @param n_intervals (integer(1)): Number of intervals.
   #'
   #' @return bounds (vector): Values of bounds with `n_intervals`+1 entries.
-  #'
-  x_s = X[, s]
-  x_s_min = min(x_s)
-  x_s_max = max(x_s)
-  bounds = seq(x_s_min, x_s_max, length.out = n_intervals + 1)
   
-  return(bounds)
+  ### Luckily for us the seq()-function already implements exactly what we want if we take two neighboring 
+  ### equidistant points as the boundaries of one interval. Now we need n_intervals + 1 points since we always
+  ### need two points for one interval resulting in one point more than the number of intervals we want.
+  seq(min(X[ , s]), max(X[ , s]), length.out = n_intervals + 1)
 }
 
 calculate_ale = function(model, X, s, n_intervals = 100, centered = FALSE) {
@@ -32,73 +30,62 @@ calculate_ale = function(model, X, s, n_intervals = 100, centered = FALSE) {
   #' @return a list with the following elements: 
   #' bounds (vector): See function `get_bounds`.
   #' ale (vector): Values of ALE.
-
-  # Get the feature column specified by s from the input data.
-  x_s = X[, s]
   
-  # Get the interval bounds with the lowerbound and upperbound specified.
-  bounds = get_bounds(X, s, n_intervals)
-  boundsLength = length(bounds)
-  lowerbound = bounds[1:boundsLength - 1]
-  upperbound = bounds[2:boundsLength]
+  ### Get the feature column specified by s from the input data.
+  x_s <- X[, s]
   
-  # Calculate_ale function later passed to mapply()
-  uncentered_ale_calculation = function(i1, i2) {
-    # Get ids in between the interval
-    if (i1 == lowerbound[1]) {
-      idx = which(x_s >= i1 & x_s <= i2)
+  ### Save all possible lower and upper bounds in lowerbounds and upperbounds, respectively. 
+  boundary_points <- get_bounds(X, s, n_intervals)
+  lowerbounds <- boundary_points[1:n_intervals]
+  upperbounds <- boundary_points[2:(n_intervals + 1)]
+  
+  ### Now we want to iterate over all intervals. For this we will use mapply() in the following but for readability we define the
+  ### function to apply before. This function returns the uncentered ALE value for one interval given the left and the right
+  ### boundary of the interval.
+  uncentered_ale_calculation <- function(left_boundary, right_boundary) {
+    ### The first interval is of the form [left_boundary, right_boundary] (so that we do not ignore the points with the minimal feature value).
+    if (left_boundary == lowerbounds[1]) {
+      idxs <- which(x_s >= left_boundary & x_s <= right_boundary)
     }
-    else{
-      idx = which(x_s > i1 & x_s <= i2)
+    ### All other intervals are of the form (left_boundary, right_boundary] (so that no point falls into two intervals).
+    else {
+      idxs <- which(x_s > left_boundary & x_s <= right_boundary)
     }
     
-    if (length(idx) == 0) {
+    ### If there is no point in the interval return 0 (as stated in the task).
+    if (length(idxs) == 0) {
       return(0)
     }
     
-    # Now we replace the values at position s one time with smallest value
-    # and one time with the highest value in this interval.
-    X_min = X[idx,]
-    X_max = X[idx,]
-    X_min = replace(X_min, s, i1)
-    X_max = replace(X_max, s, i2)
+    ### Else we compute the ALE value. The first step is to replace the feature values of all the points that fall into the interval
+    ### with the left and the right boundary of the interval.
+    X_min <- X[idxs, ]
+    X_max <- X[idxs, ]
+    X_min <- replace(X_min, s, left_boundary)
+    X_max <- replace(X_max, s, right_boundary)
     
-    # And get the new predictions
-    y_min = predict(model, X_min)
-    y_max = predict(model, X_max)
+    ### Then we let the model predict these "new" data points.
+    y_min <- predict(model, X_min)
+    y_max <- predict(model, X_max)
     
-    # Calculate difference now
-    diff = y_max - y_min
-    uncentered_ale_k = sum(diff) / length(idx)
-    return(uncentered_ale_k)
+    ### We return the arithmetic mean of the prediction differences.
+    mean(y_max - y_min)
   }
-  # Use mapply() to loop through each interval
-  result = mapply(FUN = uncentered_ale_calculation, lowerbound, upperbound)
   
-  # Now accumulate
-  uncentered_ale = cumsum(result)
+  ### Now we can calculate the ALE values all at once with the mapply()-function. We accumulate them via the cumsum()-function.
+  ale_values <- cumsum(mapply(uncentered_ale_calculation, lowerbounds, upperbounds))
   
-  if (centered == TRUE) {
-    # Center ALE here
-    centered_ale = c()
-    mean_uncentered_ale = sum(uncentered_ale) / length(uncentered_ale)
-    
-    for (uncentered_ale_k in uncentered_ale) {
-      centered_ale_k = uncentered_ale_k - mean_uncentered_ale
-      centered_ale = c(centered_ale, centered_ale_k)
-    }
-    # Return the final results of bounds and
-    # centered_ale/uncentered_ale as a list.
-    return(list(bounds = bounds, ale = centered_ale))
-  }
-  else{
-    return(finalResult = list(bounds = bounds, ale = uncentered_ale))
-  }
+  ### If the centered argument is set to TRUE the user wants us to center the ALE values.
+  if (centered)
+    ale_values <- ale_values - mean(ale_values)
+  
+  ### Return the boundary points as well as the centered or uncentered ALE values.
+  list(bounds = boundary_points, ale = ale_values)
 }
 
 prepare_ale = function(model, X, s, n_intervals = 100, centered = TRUE) {
   #' Uses `calculate_ale` to prepare x and y data, which can be used
-  #' by matplotlib directly.
+  #' by ggplot2 directly.
   #'
   #' @param model: Classifier which has a predict method.
   #' @param X (data.frame): Input data.
@@ -111,64 +98,62 @@ prepare_ale = function(model, X, s, n_intervals = 100, centered = TRUE) {
   #'     * y: ALE values.
   #'
   
-  # Calculate ALE first.
-  ale = calculate_ale(model, X, s, n_intervals, centered)
+  ### Get the ALE values via our function from task b).
+  ale <- calculate_ale(model, X, s, n_intervals, centered)
   
-  # Get the bounds and y from the list.
-  bounds = array(unlist(ale[1]))
-  y = array(unlist(ale[2]))
+  ### Get the bounds and y from the list.
+  boundary_points <- array(unlist(ale[1]))
+  y <- array(unlist(ale[2]))
   
-  # Set up lowerbound and upperbound.
-  boundsLength = length(bounds)
-  lowerbound = bounds[1:boundsLength - 1]
-  upperbound = bounds[2:boundsLength]
+  ### Reconstruct the lowerbounds and upperbounds vectors.
+  lowerbounds <- bounds[1:n_intervals]
+  upperbounds <- bounds[2:(n_intervals + 1)]
   
-  # Get x for the centers of two bounds.
-  x = rowMeans(cbind(lowerbound, upperbound))
+  ### Get the center of each interval.
+  x <- rowMeans(cbind(lowerbounds, upperbounds))
   
-  # Return the final result as a list.
-  finalResult = data.frame(x = x,
-                      y = y)
-  
-  return(finalResult)
-  
+  ### Now for ggplot2 the expected format is a data.frame.
+  data.frame(x = x, y = y)
 }
 
-if (FALSE) {
+### A function to create the ALE plot from the previous generated objects.
+ale_plot <- function() {
   library(randomForest)
   library(ggplot2)
   
-  # Set up your working directory using swd() and get the dataset file. 
-  df = read.csv(file = 'datasets/wheat_seeds.csv')
+  ### Set up your working directory using swd() and get the dataset file. 
+  df <- read.csv(file = 'datasets/wheat_seeds.csv')
   
-  # Split the dataset to 70% train data and 30% test data.
+  ### Split the dataset to 70% train data and 30% test data.
   set.seed(100)
-  train = sample(nrow(df), 0.7 * nrow(df), replace = FALSE)
-  trainData = df[train, ]
-  testData = df[-train, ]
+  train <- sample(nrow(df), 0.7 * nrow(df), replace = FALSE)
+  trainData <- df[train, ]
+  testData <- df[-train, ]
   
-  # Normalize the target to be between 0 and 1.
-  min_max_norm = function(x) {
+  ### Normalize the target to be between 0 and 1.
+  min_max_norm <- function(x) {
     (x - min(x)) / (max(x) - min(x))
   }
   
-  trainData$Type = min_max_norm(trainData$Type)
+  trainData$Type <- min_max_norm(trainData$Type)
   
-  # Build a Random Forest model.
-  model = randomForest(Type ~ ., data = trainData, mtry = 4, importance = TRUE)
+  ### Build a Random Forest model.
+  model <- randomForest(Type ~ ., data = trainData, mtry = 4, importance = TRUE)
   
-  # Use the first feature from the dataset and set up
-  # 4 intervals to test the get_bounds function
-  bounds = get_bounds(df, 1, 4)
+  ### Use the first feature from the dataset and set up
+  ### 4 intervals to test the get_bounds function
+  bounds <- get_bounds(df, 1, 4)
   
-  # Test the calculate_ale function, with centered = FALSE
-  uncentered_ale = calculate_ale(model, df, 1, 4, FALSE)
+  ### Test the calculate_ale function, with centered = FALSE
+  uncentered_ale <- calculate_ale(model, df, 1, 4, FALSE)
   
-  #Test the prepare_ale function, with centered = TRUE
-  prepared_ale = prepare_ale(model, df, 1, 4, TRUE)
+  ### Test the prepare_ale function, with centered = TRUE
+  prepared_ale <- prepare_ale(model, df, 1, 4, TRUE)
   
   ggplot(data = prepared_ale, mapping = aes(x = x, y = y)) + 
     geom_line() + 
     geom_point()
 }
 
+### The warning message can be ignored here.
+ale_plot()
