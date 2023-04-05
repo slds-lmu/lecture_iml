d8ee0ab7cfed607468d56b6fed76d2df75acb78f
Giuseppe Casalicchio <giuseppe.casalicchio@stat.uni-muenchen.de>
2023-04-05 14:58:39 +0200

    fix_party_tree

--- slides/02_interpretable-models/rsrc/fig_ctree_bike.R
+++ slides/02_interpretable-models/rsrc/fig_ctree_bike.R
@@ -14,20 +14,27 @@ dat = cbind(X, y)
 
 ## ctree
 c_tree = ctree(y~., data = dat, 
-               control = ctree_control(maxdepth = 3, maxsurrogate = 0))
-pdf("../figure/bike_ctree.pdf", width = 6, height = 4)
+               control = ctree_control(maxdepth = 2, maxsurrogate = 0))
+pdf("../figure/bike_ctree.pdf", width =11, height = 7)
 plot(c_tree)
 dev.off()
 
+# rmse
+pred = predict(c_tree, newdata = dat)
+rmse = mean((pred-dat$y)^2)/2
 
 ## mob
 func <- function(y, x, start = NULL, weights = NULL, offset = NULL) {
   glm(y ~ x, start = start)
   }
 
-mob_tree = mob(y ~ yr | weekday + mnth + weathersit + workingday + holiday, 
+mob_tree = mob(y ~ weathersit + holiday | yr + weekday + mnth + workingday, 
                data = dat, fit = func,
                control = mob_control(maxdepth = 3))
-pdf("../figure/bike_mob.pdf", width = 6, height = 4)
+pdf("../figure/bike_mob.pdf", width = 22, height = 7)
 plot(mob_tree)
 dev.off()
+
+# rmse
+pred2 = predict(mob_tree, newdata = dat)
+rmse2 = mean((pred2-dat$y)^2)/2
