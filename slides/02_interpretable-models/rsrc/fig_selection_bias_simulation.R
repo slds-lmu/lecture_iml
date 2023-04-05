b3629e9e83e0fd260c4f1ffe94080d94a5264050
giuseppec <giuseppe.casalicchio@gmail.com>
2023-04-05 17:13:27 +0200

    improve fig size

--- slides/02_interpretable-models/rsrc/fig_selection_bias_simulation.R
+++ slides/02_interpretable-models/rsrc/fig_selection_bias_simulation.R
@@ -13,12 +13,12 @@ x = data.frame(x1 = round(rnorm(n, 0, 1), 3),
 
 ## normal tree
 tree = rpart(y~x1+x2+x3, data = x)
-pdf("../figure/selection_bias_simulation_tree.pdf", width = 11, height = 6)
+pdf("../figure/selection_bias_simulation_tree.pdf", width = 10, height = 5.5)
 #plot(as.party(tree))
 ggparty(as.party(tree),
-  terminal_space = 0.3) +
+  terminal_space = 0.2) +
   geom_edge(size = 1.5) +
-  geom_edge_label(colour = "black", size = 3, shift = 0.5) +
+  geom_edge_label(colour = "black", size = 4, shift = 0.6) +
   geom_node_plot(gglist = list(geom_boxplot(aes(y = y), alpha = 0.5),
     theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(), axis.text.y = element_text(angle = 90, hjust = 0.5))),
     scales = "fixed",
@@ -32,8 +32,8 @@ ggparty(as.party(tree),
   geom_node_label(aes(col = splitvar),
     line_list = list(aes(label = paste("Node", id)),
       aes(label = splitvar)),
-    line_gpar = list(list(size = 8, col = "black", fontface = "bold"),
-      list(size = 10)),
+    line_gpar = list(list(size = 10, col = "black", fontface = "bold"),
+      list(size = 12)),
     ids = "inner") +
   geom_node_label(aes(label = paste0("Node ", id, ", N = ", nodesize)),
     fontface = "bold",
