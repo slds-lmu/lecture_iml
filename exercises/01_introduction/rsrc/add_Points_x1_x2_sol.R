f75985a1380c17e4155b7a96e019a4104e2bf369
Fiona K. Ewald <57101697+fionawald@users.noreply.github.com>
2023-04-18 17:26:47 +0200

    update_ex1_tasks_1_2

--- exercises/01_introduction/rsrc/add_Points_x1_x2_sol.R
+++ exercises/01_introduction/rsrc/add_Points_x1_x2_sol.R
@@ -4,9 +4,9 @@ library(xtable)
 
 # DATA -------------------------------------------------------------------------
 set.seed(1234L)
-x1 = seq(-1, 1, 0.2)
-x2 = x1^2 + rnorm(length(x1), sd = 0.04)
-y = 5 * x1 + -2 * x2 + rnorm(length(x1))
+x1 = round(seq(-1, 1, 0.25), 2)
+x2 = round(x1^2+rnorm(length(x1), sd = 0.04), 2)
+y = round(5*x1 + -2*x2 + rnorm(length(x1)), 2)
 d = data.frame(y, x1, x2)
 
 # PLOT -------------------------------------------------------------------------
@@ -14,4 +14,4 @@ ggplot(d, aes(x = x1, y = x2)) +
   geom_point() +
   theme_bw()
 
-ggsave("exercises/feature-effects/figure/add_Points_x1_x2_sol.pdf", width = 3, height = 2)
+ggsave("exercises/01_introduction/figure/add_Points_x1_x2_sol.pdf", width = 3, height = 2)
