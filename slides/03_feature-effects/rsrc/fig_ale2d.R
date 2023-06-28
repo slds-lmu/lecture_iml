b2c2c986e38922f8d91e24a4e46ebab81d6c823c
jakobCampus <123662159+jakobCampus@users.noreply.github.com>
2023-06-28 15:22:50 +0200

    Update fig_ale2d.R

--- slides/03_feature-effects/rsrc/fig_ale2d.R
+++ slides/03_feature-effects/rsrc/fig_ale2d.R
@@ -3,7 +3,7 @@
 library(ggplot2)
 library(patchwork)
 theme_set(theme_bw() + theme(plot.margin=grid::unit(c(1,5.5,1,1), "pt")))
-source("slides/feature-effects/rsrc/anova_bike.R")
+source("anova_bike.R")
 
 # DATA -------------------------------------------------------------------------
 
@@ -15,4 +15,4 @@ ale = FeatureEffect$new(pred.bike, feature = c("temp", "hum"), method = "ale", g
 pdp_plot <- pdp$plot() + theme(legend.position = "top", legend.key.width = unit(1.5,"cm")) + ggtitle("Bivariate PD") + scale_fill_viridis_b(expression(hat(y)))
 ale_plot <- ale$plot() + theme(legend.position = "top", legend.key.width = unit(1.5,"cm")) + ggtitle("Second order ALE") + scale_fill_viridis_b("ALE")
 
-ggsave("slides/feature-effects/figure/ale2d.pdf", pdp_plot + ale_plot, width = 8, height = 4)
+ggsave("../figure/ale2d.pdf", pdp_plot + ale_plot, width = 8, height = 4)
