aae6aaa6017ddddbe0e895390133ca36143874d1
jakobCampus <123662159+jakobCampus@users.noreply.github.com>
2023-06-28 16:28:14 +0200

    Update ice_bike10obs.R

--- slides/03_feature-effects/rsrc/ice_bike10obs.R
+++ slides/03_feature-effects/rsrc/ice_bike10obs.R
@@ -1,7 +1,8 @@
 # PREREQ -----------------------------------------------------------------------
 
 library(ggplot2)
-source("slides/feature-effects/rsrc/anova_bike.R")
+theme_set(theme_bw())
+source("anova_bike.R")
 
 # DATA -------------------------------------------------------------------------
 
@@ -12,4 +13,4 @@ pdp = FeatureEffect$new(pred.sub, "temp", method = "ice")
 # PLOT -------------------------------------------------------------------------
 
 p1 = pdp$plot() + scale_x_continuous('Temperature') + scale_y_continuous('Predicted bike rentals', limits = c(0, 4000))
-ggsave("slides/feature-effects/figure/ice_bike10obs.jpg", p1)
+ggsave("../figure/ice_bike10obs.jpg", p1)
