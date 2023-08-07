35758bd9c56d53d531b446b2c7c4f7d034c38eb6
jakobCampus <123662159+jakobCampus@users.noreply.github.com>
2023-08-07 12:49:29 +0200

    Update old_importance-vs-effects.R

--- slides/05_feature-importance/rsrc/old_importance-vs-effects.R
+++ slides/05_feature-importance/rsrc/old_importance-vs-effects.R
@@ -1,11 +1,12 @@
 library("iml")
 library("randomForest")
 library("mlr")
+theme_set(theme_bw())
 
 # setwd('~/university/phd/2021/teaching/lecture_iml/slides/feature-importance/rsrc')
 
 set.seed(123)
-load("data/bike.RData")
+load("bike.RData")
 bike = na.omit(bike)
 task = makeRegrTask(data = bike, target = "cnt")
 mod = train("regr.randomForest", task)
@@ -15,8 +16,8 @@ imp <- FeatureImp$new(predictor, loss = "mae")
 library("ggplot2")
 plot(imp)
 imp$results
-ggsave('slides/feature-importance/figure_man/bike_pfi.pdf', width=4, height=4)
+ggsave('../figure_man/bike_pfi.pdf', width=4, height=4)
 
 pdp <- FeatureEffects$new(predictor, method='pdp+ice')
 p  = pdp$plot(ncols=3) + theme_bw()
-ggsave('slides/feature-importance/figure_man/bike_pdp+ice.pdf', width=12, height=8)
+ggsave('../figure_man/bike_pdp+ice.pdf', width=12, height=8)
